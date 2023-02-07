47e0ecc858c65b68b49b32bfa1468dedabb98593
Sourov <76588896+sourovdeybd71@users.noreply.github.com>
2023-02-07 19:58:42 +0600

    Add files via upload

---
+++ codeforces_1579E1.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        list<int> l;
+        vector<int> v(n);
+        for (int i = 0;i < n;i++)
+            cin >> v[i];
+ 
+        int i = 0;
+        while (i < n)
+        {
+            if (i == 0)
+            {
+                l.push_back(v[i]);
+                i++;
+            }
+            else
+            {
+                if (v[i] < l.front())
+                    l.push_front(v[i]);
+                else
+                    l.push_back(v[i]);
+                i++;
+            }
+        }
+        while (!l.empty())
+        {
+            cout << l.front() << " ";
+            l.pop_front();
+        }
+        cout << '\n';
+    }
+    return 0;
+}
