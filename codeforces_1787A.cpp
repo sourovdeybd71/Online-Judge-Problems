5a8605815b06d364412270a5b7a35e5588d4920b
Sourov <76588896+sourovdeybd71@users.noreply.github.com>
2023-02-19 22:34:10 +0600

    Add files via upload

---
+++ codeforces_1787A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        /* code */
+        int n;
+        cin>>n;
+        if(n%2==0){
+            cout<<(n/2)<<" "<<1<<"\n";
+        }
+        else{
+            cout<<-1<<"\n";
+        }
+    }
+    
+}
